9ecb68b124fb9aad0d561d0ce00f2f71b34ad2e7
MY-HERO-OP <129320317+MY-HERO-OP@users.noreply.github.com>
2023-12-11 22:17:58 +0530

    Update mystarts.py

--- ethon/mystarts.py
+++ ethon/mystarts.py
@@ -7,7 +7,7 @@ async def start_srb(event, st):
                               [Button.url("🦋 ᴜᴘᴅᴀᴛᴇ", url="https://t.me/Opleech"),
                                Button.url("🦋 sᴜᴘᴘᴏʀᴛ", url="https://t.me/WD_Topic_Group")],
                               [Button.url("𝐖𝐎𝐎𝐃𝐜𝐫𝐚𝐟𝐭", url="t.me/Farooq_is_KING")],
-                              [Button.url("𝐖𝐎𝐎𝐃𝐜𝐫𝐚𝐟𝐭,𝐬 𝐁𝐨𝐭", url="https://t.me/WD_Contact_Bot")]]) 
+                              [Button.url("𝐖𝐎𝐎𝐃𝐜𝐫𝐚𝐟𝐭,𝐬 𝐁𝐨𝐭", url="t.me/WD_Contact_Bot")]]) 
     
 async def vc_menu(event):
     await event.edit("**VIDEO CONVERTOR v1.4**", 
